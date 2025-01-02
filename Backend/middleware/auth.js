de08b83f6c4db1cf1923fd731ddbd2ec3db88a06
Safi-sfn <mdsafiruddin24@gmail.com>
2025-01-02 16:47:51 +0530

    isStudent || isInstructor || isAdmin part Done

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -18,7 +18,7 @@ exports.auth = async (req,res,next) => {
 
          //verify the token
          try {
-            const decode = await jwt.verify(token, process.env.JWT_SECRET)
+            const decode =  jwt.verify(token, process.env.JWT_SECRET)
             console.log(decode)
             req.user = decode
          } catch (error) {
@@ -39,9 +39,59 @@ exports.auth = async (req,res,next) => {
 
 
 //isStudent
+exports.isStudent = async (req,res,next) => {
+    try {
+        if(req.user.accountType !== "Student"){
+            return res.status(401).json({
+                success:false,
+                message:"This is for Student!"
+            })
+        }
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:"Something went wrong"
+        })
+    }
+}
 
 //isInstructor
+exports.isInstructor = async (req,res,next) =>{
+    try {
+        if(req.user.accountType !== "Instructor"){
+            return res.status(401).json({
+                success:false,
+                message:"This is for Instructor"
+            })
+        }
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:"Something went wrong"
+        })
+    }
+}
 
 //isAdmin
+exports.isAdmin = async (req,res,next) =>{
+    try {
+        if(req.user.accountType !== "Admin"){
+            return res.status(401).json({
+                success:false,
+                message:"This is for Admin"
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success:false,
+            message:"Something Went wrong"
+        })
+    }
+}
 
 
