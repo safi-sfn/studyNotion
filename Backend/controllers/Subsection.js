6cbb09c7f6bff146784f788b6500661f9c480c16
Safi-sfn <mdsafiruddin24@gmail.com>
2025-01-06 20:05:16 +0530

    subSection created done

---
+++ Backend/controllers/Subsection.js
@@ -0,0 +1,60 @@
+const SubSection = require("../models/SubSection")
+const Section = require("../models/Section")
+const { uploadImageToCloudinary } = require("../utils/imageUploader")
+require("dotenv").config()
+
+//create subSection
+
+exports.createSubSection = async (req,res) => {
+    try {
+        //data fetch from req body
+        const {sectionId,title,description,timeDuration} = req.body
+
+        //extract file/video
+        const video = req.files.videoFile
+
+        //data validation 
+        if(!sectionId || !title || !description || !timeDuration ||!video){
+            return res.status(400).json({
+                sucess:false,
+                message:"All fields are required"
+            })
+        }
+
+        //upload video to cloudinary
+        const uploadDetails = await uploadImageToCloudinary(video,process.env.FOLDER_NAME)
+
+        //create a sub section
+        const subSectionDetails = await SubSection.create({
+            title:title,
+            description:description,
+            timeDuration:timeDuration,
+            video:uploadDetails.secure_url,
+        })
+
+        //update section with this Sub section objectId
+        const updatedSection = await Section.findByIdAndUpdate(
+                                {id:sectionId},
+                                {$push:{
+                                    subSection:subSectionDetails._id
+                                }},
+                                {new:true}
+        )
+        //TODO: log updated section here, after adding populate query
+        //return respnse 
+        return res.status(200).json({
+            sucess:true,
+            message:"Sub section created successfully",
+            updatedSection
+        })
+    } catch (error) {
+        return res.status(500).json({
+            sucess:false,
+            message:"Internal server error",
+            error:error.message
+        })
+    }
+}
+
+//TODO: update subSection
+//TODO: delete subSection
