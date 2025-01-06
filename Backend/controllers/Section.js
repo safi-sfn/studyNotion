63a63ed3ff0b9ea3eaaeb28a3ebb804bad797722
Safi-sfn <mdsafiruddin24@gmail.com>
2025-01-06 18:52:29 +0530

    Section handler Done

---
+++ Backend/controllers/Section.js
@@ -0,0 +1,107 @@
+const Section = require("../models/Section")
+const Course = require("../models/Course")
+
+//create Section handler
+exports.createSection = async (req,res) =>{
+    try {
+
+        //data fetch
+        const {sectionName,courseId} = req.body
+
+        //data validation
+        if(!sectionName || !courseId){
+            return res.status(400).json({
+                success:false,
+                message:"Please fill all the fields"
+            })
+        }
+        
+        //create new Section
+        const newSection = await Section.create({sectionName})
+
+        //update course with new section ObjectID
+        const updatedCourseDetails = await Course.findByIdAndUpdate(
+                                        courseId,
+                                        {
+                                            $push:{
+                                                courseContent:newSection._id
+                                            }
+                                        },
+                                        {new:true}
+        ) 
+        //HW:use populate to replace Section/sub-section both in the updatedCourseDetails
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"Section created successfully",
+            updatedCourseDetails
+        })
+
+    } catch (error) {
+        return res.staus(500).json({
+            success:false,
+            message:"Unable to create Section",
+            error:error.message
+        })
+    }
+}
+
+
+
+//update Section handler
+
+exports.updateSection = async (req,res) => {
+    try {
+        //data fetch
+        const {sectionName,sectionId} = req.body
+
+        //data validation
+        if(!sectionName || !sectionId){
+            return res.status(400).json({
+                success:false,
+                message:"Please fill all the fields"
+            })
+        }
+
+        //update data
+        const sectionUpdate = await Section.findOneAndUpdate(sectionId, {sectionName}, {new:true})
+
+        //return res
+        return res.status(200).json({
+            success:true,
+            message:"Section updated successfully"
+            
+        })
+        
+    } catch (error) {
+        return res.staus(500).json({
+            success:false,
+            message:"Unable to create Section",
+            error:error.message
+        })
+    }
+}
+
+
+//delete section handler
+exports.deleteSection = async (req,res) => {
+    try {
+        //get Id
+        const {sectionId} = req.params
+
+        //use findByIdandDelete
+         await Section.findByIdAndDelete(sectionId)
+
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"Section deleted successfully"
+        })
+    } catch (error) {
+        return res.staus(500).json({
+            success:false,
+            message:"Unable to create Section",
+            error:error.message
+        })
+    }
+}
