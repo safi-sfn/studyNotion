acfc34017b20842dec9ef8eeae56126322760345
Safi-sfn <mdsafiruddin24@gmail.com>
2025-01-27 16:38:56 +0530

    adding Console

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,13 +1,12 @@
-const { config } = require("dotenv")
 const mongoose = require("mongoose")
-require("dotenv")/config()
+require("dotenv").config()
 
 exports.connect = ()=>{
     mongoose.connect(process.env.MONGODB_URL)
     .then(() => console.log("DB Connection Successfully"))
     .catch((error)=>{
         console.log("DB Connection failed")
-        console.error("error")
+        console.error("Backend/config/database.js",error)
         process.exit(1)
     })
 }
